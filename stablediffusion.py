2a222dc4a83bb4c730c7d1d78fabd7c86e4da03f
cassandrascarpa <cls354@cornell.edu>
2022-11-04 12:16:14 -0700

    Cleanup, adjust stability

--- stablediffusion.py
+++ stablediffusion.py
@@ -23,48 +23,31 @@ path = '/sdapi/v1/img2img'
 
 live_generation = True
 interactive = True
-fullscreen = False
+fullscreen = True
 tth = 3 # time to horse
 
 save_generated_images = True
 output_dir = "out/"
-# Load existing images
 saved_dir = "saved/"
-saved_filename_pattern = "generated_{}.png"
 
 base_image = "horse.jpeg"
 stable_wordlist = "stable_wordlist.txt"
 unstable_wordlist = "unstable_wordlist.txt"
+
 height = 896
 width = 960
 screenwidth = width #1920
 screenheight = height #1088
 
-prompt = "medium-size short robotic (((((horse))))) standing on four legs facing towards the camera, futuristic, steampunk, (((cyberpunk))), sci-fi, lights"
-negative_prompt = "off-center, long, (((tall))), huge, hiding, obscured, off-screen, outdoors, window, tree, city, background, human, person, rider, toy, abstract, platform, jumping, fence, gate"
-
-mode_inpaint = 1
 method = "Heun"
 sampling_steps = 30
 cfg_scale = 23
-#denoising_strength = 0.9
-mask_blur = 30
 inpaint_full_res = False
-padding = 32
-mask_type = "Draw mask"
-masked_content = "fill"
 resize = "Just resize"
-inpaint = "Inpaint masked"
-batch_count = 1
-batch_size = 1
-style1 = "None"
-style2 = "None"
-seed = -1
 
 # ---------- HORSE CODE  ----------
 img = None
 canvas = None
-label = None
 horse_container = None
 horse_info = None
 stability = 10
@@ -78,10 +61,6 @@ with open(stable_wordlist) as sw:
 
 with open(unstable_wordlist) as uw:
     unstable_words = [line.rstrip() for line in uw]
-
-def json_to_string(j):
-    vals = list(j.values())
-    return "={},".join(j.keys()).format(*vals) + "={}".format(vals[-1])
 
 def update_image(newhorse):
    global img
@@ -127,6 +106,8 @@ def generate_prompt(stability):
     num_unstable_words = math.ceil((10-stability)/2)
     if stability < 4:
         num_unstable_words += 1
+    if stability > 5 and num_unstable_words > 0:
+        num_unstable_words -= 1
     for _ in range(num_stable_words):
         prompt.append(random.choice(stable_words))
     for _ in range(num_unstable_words):
@@ -221,8 +202,7 @@ def main():
     update_info()
 
     if interactive:
-        t1 = threading.Thread(target=repl)
-        
+        t1 = threading.Thread(target=repl)   
     else:
         t1 = threading.Thread(target=destabilizer)
 
@@ -240,7 +220,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# ---------- TODOS are back and they're better than ever -------------
-# Set stability level with hardware
-# Make sure everything works on the pi
